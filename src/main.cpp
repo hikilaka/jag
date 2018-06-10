a9a87b3ce17d02a0f0c4ef1d3068bd9ef0f58997
Zack Penn <i-need-fire@hotmail.com>
2018-06-09 23:54:54 -0500

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,18 @@
 #include <sysd/buffer.hpp>
 #include <sysd/jag/archive.hpp>
 
+#include <boost/filesystem.hpp>
+#include <boost/optional.hpp>
 #include <boost/program_options.hpp>
+#include <boost/stacktrace.hpp>
 
-sysd::buffer read_file(const std::string name) {
+boost::optional<sysd::buffer> read_file(const std::string name) {
     std::ifstream file(name, std::ios::binary);
+
+    if (!file.is_open() && file.good()) {
+        return boost::none;
+    }
+
     file.unsetf(std::ios::skipws);
     file.seekg(0, std::ios::end);
 
@@ -27,82 +35,147 @@ sysd::buffer read_file(const std::string name) {
     return sysd::buffer{data};
 }
 
-void write_file(const std::string name, const sysd::buffer &buffer) {
-    std::ofstream out{name};
+void write_file(const boost::filesystem::path &location,
+                const sysd::buffer &buffer) {
+    std::ofstream out{location.string()};
     std::ostream_iterator<char> out_itr{out};
 
     std::copy(std::cbegin(buffer.data()), std::cend(buffer.data()), out_itr);
 }
 
-// TODO: clean up the program options portion.. it's REALLY messy
-int main(int argc, char **argv) {
-    namespace opts = boost::program_options;
+void write_archive(const boost::filesystem::path &file,
+                   const sysd::jag::archive &archive) {}
 
-    opts::options_description desc{"options"};
-    desc.add_options()("help,h", "prints this help message")(
-        "verbose,v", "enables debug information")(
-        "extract,e", opts::value<std::vector<std::string>>(),
-        "extracts a given entry")("out,o", opts::value<std::string>(),
-                                  "specify output location")(
-        "input", opts::value<std::string>(), "input archive to operate on");
+namespace opts = boost::program_options;
 
-    opts::positional_options_description pos_opts;
-    pos_opts.add("input", -1);
+opts::options_description generic_opts{"generic options"};
+opts::options_description op_opts{"archive opterations"};
+opts::options_description visible_opts{"allowed options"};
+opts::options_description all_opts{"all options"};
+opts::positional_options_description hidden_opts{};
 
-    opts::variables_map var_map{};
+void init_program_options() {
+    using str_val = std::string;
+    using vec_val = std::vector<std::string>;
+
+    generic_opts.add_options()("help,h", "prints this help message")(
+        "verbose,v", "enables debug information");
+
+    op_opts.add_options()(
+        "extract,e",
+        opts::value<vec_val>()->composing()->default_value({}, "{}"),
+        "extracts an archive's entry")(
+        "insert,i",
+        opts::value<vec_val>()->composing()->default_value({}, "{}"),
+        "inserts a file into an archive")(
+        "output,o", opts::value<str_val>(),
+        "specifies the output directory/file");
+
+    hidden_opts.add("input", -1);
+
+    visible_opts.add(generic_opts).add(op_opts);
+
+    all_opts.add(visible_opts);
+    all_opts.add_options()("input", opts::value<str_val>());
+}
+
+void display_help() {
+    // TODO implement a libfmt printer for
+    // options_description in the future
+    std::stringstream ss{};
+    ss << visible_opts;
+
+    fmt::print("{}\n", ss.str());
+}
+
+opts::variables_map parse_arguments(int argc, char **argv) {
+    opts::variables_map map{};
+
     opts::store(opts::command_line_parser{argc, argv}
-                    .options(desc)
-                    .positional(pos_opts)
+                    .options(all_opts)
+                    .positional(hidden_opts)
                     .run(),
-                var_map);
-    opts::notify(var_map);
+                map);
+    opts::notify(map);
 
-    if (var_map.count("help")) {
-        std::stringstream ss{}; // really?
-        ss << desc;
-        fmt::print("{}\n", ss.str());
+    return map;
+}
+
+int main(int argc, char **argv) {
+    init_program_options();
+    const auto args = parse_arguments(argc, argv);
+
+    if (args.count("help")) {
+        display_help();
         return 0;
     }
 
-    if (var_map.count("verbose")) {
+    if (args.count("verbose")) {
         spdlog::set_level(spdlog::level::debug);
     }
 
-    if (!var_map.count("input")) {
-        fmt::print("error: no input file\n\n");
-        std::stringstream ss{};
-        ss << desc;
-        fmt::print("{}\n", ss.str());
+    auto log = spdlog::stdout_color_mt("jag");
+
+    if (!args.count("input")) {
+        log->warn("no input files\n");
+        display_help();
         return 1;
     }
 
-    if (!var_map.count("extract")) { // TODO add future operations here
-        fmt::print("no archive operation specified, at least one required");
-        return 1;
-    }
+    auto out_path = boost::filesystem::current_path();
 
-    auto log = spdlog::stdout_color_mt("console");
+    if (args.count("output")) {
+        out_path = boost::filesystem::path{args["output"].as<std::string>()};
+
+        if (!boost::filesystem::is_directory(out_path)) {
+            log->critical("supplied output directory isn't a directory");
+            return 1;
+        }
+    }
 
     try {
-        const auto archive_name = var_map["input"].as<std::string>();
+        const auto archive_name = args["input"].as<std::string>();
+        const auto req_extract = args["extract"].as<std::vector<std::string>>();
+        const auto req_insert = args["insert"].as<std::vector<std::string>>();
 
-        auto buffer = read_file(archive_name);
-        sysd::jag::archive archive{buffer};
+        if (req_extract.size() == 0 && req_insert.size() == 0) {
+            log->warn("no archive operation specified");
+            display_help();
+            return 1;
+        }
 
-        if (var_map.count("extract")) {
-            const auto files =
-                var_map["extract"].as<std::vector<std::string>>();
+        if (auto buffer = read_file(archive_name); buffer) {
+            sysd::jag::archive archive{buffer.value()};
 
-            for (const auto &file : files) {
+            for (const auto &file : req_extract) {
                 if (auto data = archive.get(file); data) {
-                    write_file(fmt::format("{}", file), data.value());
+                    write_file(out_path / file, data->data());
                 } else {
-                    log->warn("couldn't find {} in archive", file);
+                    log->warn("couldn't find {} in {}", file, archive_name);
                 }
             }
+
+            for (const auto &file : req_insert) {
+                if (auto data = read_file(file); data) {
+                    archive.put(file, data.value());
+                } else {
+                    log->warn("couldn't find {} while inserting", file);
+                }
+            }
+
+            if (req_insert.size() > 0) {
+                // write archive
+            }
+        } else {
+            log->critical("unable to open {}", archive_name);
+            return 1;
         }
     } catch (std::exception &e) {
+        auto trace = boost::stacktrace::stacktrace();
         log->critical("uncaught exception: {}", e.what());
+        std::stringstream ss{};
+        ss << trace;
+        fmt::print("{}", ss.str());
     }
     return 0;
 }
